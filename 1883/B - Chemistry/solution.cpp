f81734da53886657dcb52007da03a7922ba6813e
Harsh Detroja <hdharsh3204@gmail.com>
2026-07-01 20:12:51 +0530

    Add Chemistry [B] from Codeforces

---
+++ 1883/B - Chemistry/solution.cpp
@@ -0,0 +1,161 @@
+#include <algorithm>
+#include <iostream>
+#include <utility>
+#include <vector>
+ 
+using namespace std;
+ 
+using ll = long long;
+using vi = vector<int>;
+using vll = vector<long long>;
+using pii = pair<int, int>;
+using pll = pair<long long, long long>;
+ 
+const ll MOD = 1e9 + 7;
+const ll INF = 1e18;
+const int MAXN = 1000005;
+ 
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define input(v, n)             \
+    for (int i = 0; i < n; i++) \
+    cin >> v[i]
+ 
+struct FastIO
+{
+    FastIO()
+    {
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+    }
+} fastio;
+ 
+#ifndef ONLINE_JUDGE
+#define dbg(x)           \
+    cerr << #x << " = "; \
+    _print(x);           \
+    cerr << endl;
+#else
+#define dbg(x)
+#endif
+ 
+void _print(int x) { cerr << x; }
+void _print(long long x) { cerr << x; }
+ 
+template <class T1, class T2>
+void _print(pair<T1, T2> p)
+{
+    cerr << "{" << p.first << "," << p.second << "}";
+}
+ 
+template <class T>
+void _print(vector<T> v)
+{
+    cerr << "[ ";
+    for (T i : v)
+    {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+ 
+template <typename T>
+istream &operator>>(istream &in, vector<T> &v)
+{
+    for (auto &x : v)
+        in >> x;
+    return in;
+}
+ 
+template <typename T>
+ostream &operator<<(ostream &out, const vector<T> &v)
+{
+    for (const auto &x : v)
+        out << x << " ";
+    return out;
+}
+ 
+vector<int> primes;
+int spf[MAXN];
+ 
+void solve()
+{
+    ll n, k;
+    cin >> n >> k;
+    string s;
+    cin >> s;
+    if (n == 1 || n - k == 1)
+    {
+        cout << "YES" << endl;
+        return;
+    }
+    ll rem = n - k;
+    ll arr[26] = {0};
+    for (ll i : s)
+    {
+        arr[i - 'a']++;
+    }
+    // ll even = 0;
+    // ll odd = 0;
+    // for (ll i = 0; i < 26; i++)
+    // {
+    //     if (arr[i] % 2 != 0)
+    //     {
+    //         odd++;
+    //     }
+    // }
+    // if(odd>k){
+    //     odd-=k;
+    //     if(odd==1 && n%2-k!=0){
+    //         cout<<"YES"<<endl;
+    //         return;
+    //     }else{
+    //         cout<<"NO"<<endl;
+    //         return;
+    //     }
+    // }else{
+    //     k-=odd;
+    //     if(k%2==0 || (k%2!=0 && n%2-k!=0)){
+    //         cout<<"YES"<<endl;
+ 
+    //     }else{
+    //         cout<<"NO"<<endl;
+    //     }
+    // }
+    // if (odd > k){
+    //     odd -= k;
+    //     if (odd == 1 && (n - k) % 2 != 0)
+    //         cout << "YES
+";
+    //     else
+    //         cout << "NO
+";
+    // }
+    // else{
+    //     k -= odd;
+    //     if (k % 2 == 0 || (k % 2 != 0 && (n - k) % 2 != 0))
+    //         cout << "YES
+";
+    //     else
+    //         cout << "NO
+";
+    // }
+ 
+    int odd = 0;
+    for (int i = 0; i < 26; i++)
+        if (arr[i] & 1)
+            odd++;
+    cout << (odd <= k + 1 ? "YES" : "NO") << '
+';
+ 
+}
+ 
+signed main()
+{
+    ll T = 1;
+    cin >> T;
+    while (T--)
+        solve();
+    return 0;
+}
